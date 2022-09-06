33d0679e687b3c4743853c1a3f2c67b917ae057f
gabrielback12 <gabrielluiz-back@hotmail.com>
2022-09-06 00:15:17 -0300

    Crud Scaffolding

---
+++ Data/SalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ControleDeVendas.Models;
+
+namespace ControleDeVendas.Data
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ControleDeVendas.Models.Department> Department { get; set; } = default!;
+    }
+}
