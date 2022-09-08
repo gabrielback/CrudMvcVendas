c07b22c305a6fdb1e827c1084d29ba3c2e39d1e8
gabrielback12 <gabrielluiz-back@hotmail.com>
2022-09-07 21:00:12 -0300

    Sellers Controller

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeVendas.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
