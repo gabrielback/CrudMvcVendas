7a7c53abd3341258c0a7c0b48148f29bee517078
gabrielback12 <gabrielluiz-back@hotmail.com>
2022-09-11 01:37:48 -0300

    Returning custom error page

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using ControleDeVendas.Models.ViewModels;
 using ControleDeVendas.Services;
 using ControleDeVendas.Services.Exceptions;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace ControleDeVendas.Controllers
 {
@@ -48,14 +49,14 @@ namespace ControleDeVendas.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Id not provided"});
             }
             // pegar quem é o obj que sera deletado
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Seller not found"});
             }
 
             return View(obj);
@@ -74,14 +75,14 @@ namespace ControleDeVendas.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             // pegar quem é o obj que sera deletado
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
@@ -91,12 +92,13 @@ namespace ControleDeVendas.Controllers
         {
             if(id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+
             }
             var obj = _sellerService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             List<Department> departments = _departmentService.FindAll();
@@ -110,22 +112,39 @@ namespace ControleDeVendas.Controllers
         {
             if(id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
             try
             {
             _sellerService.update(seller);
             return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch(ApplicationException e) // supertipo 
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+ 
+            /*
+             * Desnecessário, uma vez que pode ser tratado com o supertipo ApplicationException
+            catch (NotFoundException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
             catch (DbCuncurrencyException e)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
+            */
         }
 
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
+        }
     }
 }
